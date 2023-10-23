60cd280690e310b4e9d7de451b3d7dea4fa74f3f
Juan Carlos Hernandez <hernandezjuanjc@gmail.com>
2023-10-23 11:05:58 -0500

    simple functions

---
+++ Functions/simple_functions.js
@@ -0,0 +1,13 @@
+
+function favoriteThings(){
+     console.log("Carlos", "Good Will Hunting", "Joji");
+}
+function whyImHere(){
+     console.log("To make good money with skills I like.");
+}
+function favoritePlace(){    
+     console.log("Matamoros Mexico, to see my family and my old town.");
+}
+favoriteThings();
+whyImHere();
+favoritePlace();
