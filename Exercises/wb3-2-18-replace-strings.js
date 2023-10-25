a9a2b58fe8d92abfe6b9998b319de82a7fc150ef
Juan Carlos Hernandez <hernandezjuanjc@gmail.com>
2023-10-25 13:39:08 -0500

    time

---
+++ Exercises/wb3-2-18-replace-strings.js
@@ -0,0 +1,11 @@
+let inputs = "San Francisco,Dallas,Atlanta,Hartford";
+
+
+let array = inputs.split(",");
+
+for (let i = 0; i < array.length; i++) { //i is for index AKA offset 
+ replace = ("Dallas", "Austin")
+console.log(array[i]);
+
+}
+console.log("ALL DONE");
