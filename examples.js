5e1894df39014e615ce11352b137edb8131b9c90
Juan Carlos Hernandez <hernandezjuanjc@gmail.com>
2023-10-26 10:48:52 -0500

    fixes

--- examples.js
+++ examples.js
@@ -1,2 +1,7 @@
-let d = new Date();
-console.log(d);
+let date1 = new Date("January 15, 2022");
+let date2 = new Date("March 15, 2022");
+let msec_per_day = 1000 * 60 * 60 * 24;
+let elapsedMilliseconds = date2.getTime() - date1.getTime();
+let dayDiff = elapsedMilliseconds / msec_per_day;
+let numDays = Math.round(dayDiff);
+console.log("The number of days between dates is " + numDays);
